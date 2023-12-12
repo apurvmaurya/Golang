82a29c74a91ce0c9c4a6ded2b89d69ef2fc0f19e
apurvmaurya <apurvmaurya5@gmail.com>
2023-12-13 02:58:56 +0530

    temp

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module myModule
 
 go 1.21.5
+
+require github.com/gitApurv/puppy v0.0.0-20231212212350-2327bb8fe5ef
+
+require github.com/gitApurv/dog v0.0.0-20231212211920-d6973e4e57b1 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gitApurv/puppy"
 )
 
 func main() {
 	fmt.Println("Hello Gophers")
+	fmt.Println(puppy.PuppyName)
+
+	fmt.Println(puppy.Family("temp"))
 }
