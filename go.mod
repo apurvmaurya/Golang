56dd596c1b7626c048af79589aea9c88ef73c9bd
apurvmaurya <apurvmaurya5@gmail.com>
2023-12-13 02:38:34 +0530

    unknown

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module myModule
 
 go 1.21.5
-
-require github.com/apurvmaurya/puppy v0.0.0-20231212202115-1bf4ed768674
-
-require github.com/apurvmaurya/animal v0.0.0-20231212203017-2ad153edc7a4
--- main.go
+++ main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/apurvmaurya/puppy"
-
-	"github.com/apurvmaurya/animal"
 )
 
 func main() {
 	fmt.Println("Hello Gophers")
-	fmt.Println(puppy.Name)
-	fmt.Println(animal.Name)
 }
