23617f07d6ca87028df56f524b1a3b7b3d8e3088
apurvmaurya <apurvmaurya5@gmail.com>
2023-12-13 02:02:37 +0530

    commit

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	"github.com/apurvmaurya/puppy"
+
+	"github.com/apurvmaurya/animal"
 )
 
 func main() {
 	fmt.Println("Hello Gophers")
 	fmt.Println(puppy.Name)
+	fmt.Println(animal.Name)
 }
